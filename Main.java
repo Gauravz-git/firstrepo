55f0c871a9fd3cce52d7a6ca9277e09eb04b5aed
gauravz-git <gauravc0223@gmail.com>
2025-02-12 23:20:23 +0530

    changes where made

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main{ 
   public static void main(String[] args) {
     System.out.println("This is my first Java Program");
+    int Num1 = 21;
+    int Num2 = 32;
+    System.out.println(Num1 + Num2);
   }
 
 
